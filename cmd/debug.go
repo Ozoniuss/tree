8a75142d10592f6647359555304a5dad63d09fc8
Ozoniuss <qscxzsewqazxcde@gmail.com>
2025-05-25 17:38:22 +0300

    Port java tree printer to go
    Original implementation is at
    https://github.com/billvanyo/tree_printer/blob/master/src/main/java/tech/vanyo/treePrinter/TreePrinter.java

--- cmd/debug.go
+++ cmd/debug.go
@@ -7,10 +7,14 @@ import (
 )
 
 func main() {
+	// t := tree.NewBST[string]()
+	// t.Insert("4sfhskfuceskjvsdnkvjkdsnfvnjkdfsvnjkdfsnvdksfjvsdfvdfs")
+	// t.Insert("jsdbfalkfbdslkjfbadslkfbladsjkfnbadskljfnbadslkjfbnadsjkfblasd")
+	// t.Insert("1dsbflkjsdbfjzhklsdbfljkdsbfjkalbfjkdasbfalsdbfkjasdbfdkkfald")
+
 	t := tree.NewBST[int]()
 	t.Insert(4)
-	t.Insert(2)
-	t.Insert(12)
+
 	t.Insert(1)
 	t.Insert(8)
 
@@ -23,4 +27,12 @@ func main() {
 	t.Insert(13)
 
 	fmt.Println(tree.FormatTree(t))
+
+	t2 := tree.NewBST[int]()
+	t2.Insert(5)
+
+	p := tree.New[int]()
+	// p.SquareBranches = true
+	p.PrintTree(t.Root())
+	p.PrintTrees([]tree.Node[int]{t.Root(), t2.Root()}, 1)
 }
