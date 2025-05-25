89d82a45cf4bc46beb1f25baa7622cfd84b4e4d8
Ozoniuss <qscxzsewqazxcde@gmail.com>
2025-05-25 19:21:57 +0300

    Document tree interfaces

--- tree.go
+++ tree.go
@@ -2,18 +2,62 @@ package tree
 
 import "cmp"
 
+// Tree represents the possible operations on binary search trees. Various tree
+// types (e.g. regular BST, balanced BST, red black tree etc.) implement this
+// interface.
+//
+// Calling any method on a nil tree should panic.
 type Tree[T cmp.Ordered] interface {
+	// Retrieve the Root of this tree. Returns nil for a tree that had no nodes
+	// inserted to it.
 	Root() Node[T]
+	// Return the number of nodes in the tree.
 	Size() int
+	// Insert a value to the binary search tree. Implementations that require
+	// storing unique values will return an error if that value already exists.
+	// Callers may choose to ignore the error if they just want to ensure the
+	// value is present in the tree.
 	Insert(value T) error
-	MustInsert(value T)
+	// Count returns the number of elements with value `value` present in the
+	// tree. Implementations that require unique values will either return 0
+	// or 1.
+	//
+	// Use this method if you need to determine whether a value belongs to the
+	// tree or not.
 	Count(value T) int
 }
 
+// Node represents the possible operations on binary search tree nodes.
+//
+// Nodes are not meant to be modified directly as all operations are expected
+// to be performed using the Tree object. This is to prevent producing a tree
+// that does not satisfy the binary search tree property. Functions from this
+// interface only allow reading a node's state.
+//
+// However, implementations may decide to allow mutating nodes. If you wish to
+// modify the node's state directly, you should assert this interface into the
+// node's concrete type.
+//
+// Calling any method on a nil node should panic.
 type Node[T cmp.Ordered] interface {
+	// Parent returns the parent node. This should return nil for the root node
+	// and a non-nil value for all other nodes.
+	//
+	// This method should be used to check if an individual node is the root of
+	// the tree.
 	Parent() Node[T]
+	// Value returns the value stored in the node.
 	Value() T
+	// Left returns the left child of the node. This should return nil if there
+	// is no left child.
 	Left() Node[T]
+	// Right returns the right child of the node. This should return nil if there
+	// is no right child.
 	Right() Node[T]
+	// Count returns the number of elements equal to the node's `value` are
+	// present in the tree. Since the nodes are always bound to only one tree
+	// (i.e. nodes are not created without a tree directly or transitively
+	// pointing to them), implementations that require unique values will always
+	// return 1.
 	Count() int
 }
